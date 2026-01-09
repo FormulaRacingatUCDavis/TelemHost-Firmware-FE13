ea8734b25350c40ace2b735b4f25a847faed5081
Vi-L <viclei1216@gmail.com>
2026-01-09 14:42:52 -0800

    stop git tracking files in debug folder

--- TelemHost_Firmware_FE13/Debug/Core/Src/xsens/xsens_mdata2.d
+++
@@ -1,6 +0,0 @@
-Core/Src/xsens/xsens_mdata2.o: ../Core/Src/xsens/xsens_mdata2.c \
- ../Core/Src/xsens/xsens_mdata2.h ../Core/Src/xsens/xsens_constants.h \
- ../Core/Src/xsens/xsens_utility.h
-../Core/Src/xsens/xsens_mdata2.h:
-../Core/Src/xsens/xsens_constants.h:
-../Core/Src/xsens/xsens_utility.h:
--- TelemHost_Firmware_FE13/Debug/Core/Src/xsens/xsens_utility.d
+++
@@ -1,3 +0,0 @@
-Core/Src/xsens/xsens_utility.o: ../Core/Src/xsens/xsens_utility.c \
- ../Core/Src/xsens/xsens_utility.h
-../Core/Src/xsens/xsens_utility.h:

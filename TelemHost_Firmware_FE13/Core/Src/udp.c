85306b62e011b6354706f101f2dcd93c724dd2ff
Vi-L <viclei1216@gmail.com>
2026-02-27 14:34:35 -0800

    refactor udp server thread to use .ioc setup

--- TelemHost_Firmware_FE13/Core/Src/udp.c
+++ TelemHost_Firmware_FE13/Core/Src/udp.c
@@ -7,77 +7,41 @@
 
 // https://controllerstech.com/udp-server-using-netconn-with-rtos-in-stm32/
 
-#include "lwip.h"
-
-#include "lwip/opt.h"
-
-#include "lwip/api.h"
-#include "lwip/sys.h"
-
 #include "udp.h"
 #include "string.h"
 
-#define UDP_SERVER_PORT 7
-
-static struct netconn *conn;
-static struct netbuf *buf;
 //static ip_addr_t *addr;
 //static unsigned short port;
-char msg[100];
-char smsg[200];
+//char msg[100];
 
 /*-----------------------------------------------------------------------------------*/
-/**** Send RESPONSE every time the client sends some data ******/
-static void udp_thread(void *arg)
+/**** Send RESPONSE every time the queue has some data ******/
+void udp_update(struct netconn *conn, struct netbuf *buf, char* smsg, err_t *err, struct pbuf *txBuf)
 {
-	err_t err;
-	//err_t recv_err;
-	struct pbuf *txBuf;
-
-	/* Create a new connection identifier */
-	conn = netconn_new(NETCONN_UDP);
-
-	if (conn!= NULL)
+	if (1) // TODO if data is available in the queue
 	{
-		/* Bind connection to the port 7 */
-		err = netconn_bind(conn, IP_ADDR_ANY, UDP_SERVER_PORT);
+		// allocate new packet buffer
+		buf = netbuf_new();
 
-		if (err == ERR_OK)
-		{
-			/* The while loop will run everytime this Task is executed */
-			while (1)
-			{
-				if (1) // TODO if data is available in the queue
-				{
-					// allocate new packet buffer
-					buf = netbuf_new();
+		if (buf != NULL) {
+			// prepare message to send
+			int len = sprintf(smsg, "TODO add message \n");
 
-					if (buf != NULL) {
-						// prepare message to send
-						int len = sprintf(smsg, "TODO add message \n");
+			/* allocate pbuf from RAM*/
+			txBuf = pbuf_alloc(PBUF_TRANSPORT,len, PBUF_RAM);
 
-						/* allocate pbuf from RAM*/
-						txBuf = pbuf_alloc(PBUF_TRANSPORT,len, PBUF_RAM);
+			/* copy the data into the buffer  */
+			pbuf_take(txBuf, smsg, len);
 
-						/* copy the data into the buffer  */
-						pbuf_take(txBuf, smsg, len);
+			// refer the netbuf->pbuf to our pbuf
+			buf->p = txBuf;
 
-						// refer the netbuf->pbuf to our pbuf
-						buf->p = txBuf;
+			// broadcast the message
+			netconn_sendto(conn, buf, IP_ADDR_BROADCAST, UDP_SERVER_PORT);
 
-						// broadcast the message
-						netconn_sendto(conn, buf, IP_ADDR_BROADCAST, UDP_SERVER_PORT);
-
-						buf->addr.addr = 0;  // clear the address
-						pbuf_free(txBuf);   // clear the pbuf
-						netbuf_delete(buf);  // delete the netbuf
-					}
-				}
-			}
-		}
-		else
-		{
-			netconn_delete(conn);
+			buf->addr.addr = 0;  // clear the address
+			pbuf_free(txBuf);   // clear the pbuf
+			netbuf_delete(buf);  // delete the netbuf
 		}
 	}
 }

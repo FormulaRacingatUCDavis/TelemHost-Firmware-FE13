8d16ef5479ed32cdbaf3ecb13b97fd0552ccc08b
Vi-L <viclei1216@gmail.com>
2026-02-28 12:26:49 -0800

    minor changes

--- TelemHost_Firmware_FE13/Core/Src/main.c
+++ TelemHost_Firmware_FE13/Core/Src/main.c
@@ -1022,8 +1022,11 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 
   if(htim->Instance == TIM9){
 	  // check least significant bit because unlikely to be zero once data is sent
-	  if( LAT_LON_DATA[7] != 0){
+	  if( LAT_LON_DATA[7] + LAT_LON_DATA[6] + LAT_LON_DATA[5] + LAT_LON_DATA[4] != 0){
 		  CAN_Send(&hcan2, 0x460, LAT_LON_DATA,8);
+		  char buf[64];
+		  snprintf(buf, sizeof(buf), "Lat: %d, Lon: %d", LAT_LON_DATA[4], LAT_LON_DATA[0]);
+		  print(buf);
 	  }
 
   }

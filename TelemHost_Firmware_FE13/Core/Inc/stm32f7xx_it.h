f007cfa71d3dbf20b9f572294f459796db7b7288
Vi-L <viclei1216@gmail.com>
2026-04-22 19:35:49 -0700

    enable can1 tx interrupt in IOC

--- TelemHost_Firmware_FE13/Core/Inc/stm32f7xx_it.h
+++ TelemHost_Firmware_FE13/Core/Inc/stm32f7xx_it.h
@@ -53,6 +53,7 @@ void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
 void DMA1_Stream5_IRQHandler(void);
+void CAN1_TX_IRQHandler(void);
 void CAN1_RX0_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
 void SDMMC1_IRQHandler(void);
--- TelemHost_Firmware_FE13/Core/Src/stm32f7xx_hal_msp.c
+++ TelemHost_Firmware_FE13/Core/Src/stm32f7xx_hal_msp.c
@@ -119,6 +119,8 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan)
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* CAN1 interrupt Init */
+    HAL_NVIC_SetPriority(CAN1_TX_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(CAN1_TX_IRQn);
     HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 5, 0);
     HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
     /* USER CODE BEGIN CAN1_MspInit 1 */
@@ -182,6 +184,7 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* hcan)
     HAL_GPIO_DeInit(GPIOA, TCAN_RX_Pin|TCAN_TX_Pin);
 
     /* CAN1 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(CAN1_TX_IRQn);
     HAL_NVIC_DisableIRQ(CAN1_RX0_IRQn);
     /* USER CODE BEGIN CAN1_MspDeInit 1 */
 

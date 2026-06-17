eac6b0b2f70b5ce7f6854abb77f962fd41a98dd0
Victor Lei <viclei1216@gmail.com>
2026-06-17 13:51:51 -0700

    increase CAN sending delay to 100 ms per packet type

--- TelemHost_Firmware_FE13/Core/Src/xsens.c
+++ TelemHost_Firmware_FE13/Core/Src/xsens.c
@@ -30,7 +30,7 @@ extern CAN_HandleTypeDef hcan1;
 // PRIVATE FUNCTION PROTOTYPES
 void imu_callback(XsensEventFlag_t event, XsensEventData_t *mtdata);
 
-#define CAN_SEND_DELAY 10
+#define CAN_SEND_DELAY 100
 uint32_t prev_sends[21] = {0};
 uint32_t time_diff = 0;
 
